e2cd0f8e0afd6faeb6231fa4048dde80a2069588
Maya Dominice <maya.dominice@gmail.com>
2023-05-16 19:01:12 +1000

    Example Portfolio Code first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const hamburgerButton = document.querySelector(".hamburger-button");
+const mobileNav = document.querySelector(".mobile-nav");
+
+hamburgerButton.addEventListener("click",() => {
+    mobileNav.classList.toggle("display-none")
+    alert("helllooooo")
+    
+})
+
+
